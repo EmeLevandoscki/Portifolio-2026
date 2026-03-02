4af73eb0c4935c16902969003075387b7b554cb9
Emelly Levandoscki <99996497+EmeLevandoscki@users.noreply.github.com>
2026-03-02 10:50:03 -0300

    criado os cards de projetos, finalizado

--- script.js
+++ script.js
@@ -26,3 +26,34 @@ window.addEventListener('load', () => {
         terminal.classList.add('show-glow');
     }, 500);
 });
+
+
+//filtro de projetos 
+const botoes = document.querySelectorAll(".btn-filtro");
+const projetos = document.querySelectorAll(".projeto-card");
+
+botoes.forEach(botao => {
+    botao.addEventListener("click", () => {
+
+        // muda botão ativo
+        botoes.forEach(b => b.classList.remove("ativo"));
+        botao.classList.add("ativo");
+
+        const filtro = botao.dataset.filter;
+
+        projetos.forEach(projeto => {
+
+            const techs = projeto.dataset.tech
+                ? projeto.dataset.tech.toLowerCase().split(" ")
+                : [];
+
+            if (filtro === "all" || techs.includes(filtro.toLowerCase())) {
+                projeto.style.display = "";
+            } else {
+                projeto.style.display = "none";
+            }
+
+        });
+
+    });
+}); 
